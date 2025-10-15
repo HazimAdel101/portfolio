aeeda78e10b543e2792c8b542628fc7429093214
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2025-10-15 14:15:33 +0300

    add Badge component and enhance ButtonArrow functionality; update Home page layout

--- app/components/ButtonArrow.tsx
+++ app/components/ButtonArrow.tsx
@@ -1,8 +1,9 @@
 type Props = {
   color?: string;
+  className?: string;
 };
 
-export function ButtonArrow({ color }: Props) {
+export function ButtonArrow({ color, className }: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,6 +12,7 @@ export function ButtonArrow({ color }: Props) {
       height="24"
       color={color}
       fill="none"
+      className={className}
     >
       <path
         d="M9 6.65032C9 6.65032 15.9383 6.10759 16.9154 7.08463C17.8924 8.06167 17.3496 15 17.3496 15M16.5 7.5L6.5 17.5"
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { ButtonArrow } from "@/app/components/ButtonArrow";
-import ThemeToggle from "./components/ThemeToggle";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
   return (
-      <main className="">
-        <section id="hero" className="">
-          <Button>اطلب مشروعك الان<ButtonArrow color="#ffffff" /></Button>
-          <Button
-            variant={"outline"}
-            className="group"
-          >
-            اتصل بنا
-            <span className="transition-colors duration-200 group-hover:[&>svg]:text-white">
-              <ButtonArrow color="#7e22ce" />
-            </span>
-          </Button>
-        </section>
-      </main>
+    <main className="">
+      <section id="hero" className="">
+        <Button>
+          اطلب مشروعك الان
+          <ButtonArrow color="#ffffff" />
+        </Button>
+        <Button variant={"outline"} className="group">
+          اتصل بنا
+          <span className="transition-all duration-300 group-hover:rotate-45">
+            <ButtonArrow
+              className="transition-all duration-300 group-hover:text-white"
+              color="#7e22ce"
+            />
+          </span>
+        </Button>
+        <Badge>اطلب مشروعك الان</Badge>
+      </section>
+    </main>
   );
 }
